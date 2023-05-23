6446b1ece3c406696bf87fa82cab40ae859d4398
Yash Garg <thisisyashgarg@gmail.com>
2023-05-23 14:41:22 +0530

    postgres config file set

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
-import myDataSource from "./ormconfig";
+import AppDataSource from "./ormconfig";
+
 config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
-});
-
-// myDataSource
-//   .initialize()
-//   .then(() => {
-//     console.log("Connected to database");
-//     app.listen(PORT, () => {
-//       console.log(`Server started at http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log("TypeORM connection error: ", error);
-//   });
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,20 +1,12 @@
 import { DataSource } from "typeorm";
 
-// create a dummy data source object for psql using typeorm
-const myDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: ["src/entity/**/*.ts"],
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"],
-  cli: {
-    entitiesDir: "src/entity",
-    migrationsDir: "src/migration",
-    subscribersDir: "src/subscriber",
-  },
+  database: "test",
 });
-export default myDataSource;
+
+export default AppDataSource;
